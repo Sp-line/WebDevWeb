5911730703fdd34f79d7084ee9a415bd8357c36d
spline <dmcemin@gmail.com>
2025-11-06 15:19:38 +0100

    add db dispose to main app

--- app/main.py
+++ app/main.py
@@ -1,18 +1,30 @@
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
+
 import uvicorn
 from fastapi import FastAPI
 
+from app.database import database
 from config import settings
 from routes import router as api_router
 
-app = FastAPI()
-app.include_router(
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI) -> AsyncIterator[None]:
+    yield
+    await database.dispose()
+
+main_app = FastAPI(
+    lifespan=lifespan,
+)
+main_app.include_router(
     api_router,
     prefix=settings.api.prefix,
 )
 
 if __name__ == '__main__':
     uvicorn.run(
-        "main:app",
+        "main:main_app",
         host=settings.run.host,
         port=settings.run.port,
         reload=True,
