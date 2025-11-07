ea57d3893c813584a0cb146c228e5415a0e7dd34
spline <dmcemin@gmail.com>
2025-11-07 12:51:38 +0100

    person model use constants

--- app/models/person.py
+++ app/models/person.py
@@ -1,12 +1,13 @@
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
+from constants.person import FIRST_NAME_MAX_LEN, LAST_NAME_MAX_LEN, EMAIL_MAX_LEN
 from models import Base
 
 
 class Person(Base):
     __abstract__ = True
 
-    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
-    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
-    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(FIRST_NAME_MAX_LEN), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(LAST_NAME_MAX_LEN), nullable=False)
+    email: Mapped[str] = mapped_column(String(EMAIL_MAX_LEN), unique=True, nullable=False)
