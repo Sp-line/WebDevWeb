02ab3e39ea07add13030acc4cf6961b97d93e2b7
spline <dmcemin@gmail.com>
2025-11-07 20:10:10 +0100

    add order base, create, update schema

---
+++ app/schemas/order.py
@@ -0,0 +1,25 @@
+from decimal import Decimal
+
+from pydantic import BaseModel, Field
+
+from constants.order import TECHNICAL_TASK_MAX_LEN
+
+
+class OrderBase(BaseModel):
+    client_id: int = Field(...)
+    project_id: int = Field(...)
+    price: Decimal = Field(..., gt=0)
+    technical_task: str | None = Field(
+        None,
+        max_length=TECHNICAL_TASK_MAX_LEN,
+    )
+
+
+class OrderCreate(OrderBase):
+    pass
+
+
+class OrderUpdate(OrderBase):
+    client_id: int | None = Field(None)
+    project_id: int | None = Field(None)
+    price: Decimal | None = Field(None, gt=0)
