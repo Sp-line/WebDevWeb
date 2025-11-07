4d3a560dbdac305b22ecd5b5f891cd68db94700e
spline <dmcemin@gmail.com>
2025-11-07 20:11:23 +0100

    create project repository

---
+++ app/repositories/project.py
@@ -0,0 +1,19 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+
+from models import Project
+from schemas.project import ProjectCreate, ProjectUpdate
+from .base import RepositoryBase
+
+
+class ProjectRepository(RepositoryBase[Project, ProjectCreate, ProjectUpdate]):
+    def __init__(self, session: AsyncSession):
+        super().__init__(Project, session)
+
+    async def get_with_order(self, project_id: int) -> Project | None:
+        stmt = select(Project).where(Project.id == project_id).options(
+            selectinload(Project.order)
+        )
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
