80aa28a8a23b97d31846fd2ccd620ecb8390abdf
spline <dmcemin@gmail.com>
2025-11-08 16:08:01 +0100

    import all admin models

---
+++ app/admin/__init__.py
@@ -0,0 +1,7 @@
+from .client import ClientAdmin
+from .employee import EmployeeAdmin
+from .order import OrderAdmin
+from .project import ProjectAdmin
+from .team import TeamAdmin
+
+__all__ = ["ProjectAdmin", "ClientAdmin", "OrderAdmin", "EmployeeAdmin", "TeamAdmin"]
