1b99e363e4d46a13f44fdb045afbc6ac4b75e525
spline <dmcemin@gmail.com>
2025-11-08 16:05:35 +0100

    add client admin

---
+++ app/admin/client.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from sqladmin import ModelView
+
+from admin.core import register_admin
+from models import Client
+from schemas.person import PersonBase
+
+if TYPE_CHECKING:
+    from typing import Any
+    from fastapi import Request
+
+
+@register_admin
+class ClientAdmin(ModelView, model=Client):
+    column_list = [
+        Client.id,
+        Client.first_name,
+        Client.last_name,
+        Client.email
+    ]
+
+    async def on_model_change(self, data: dict[str, Any], model: Any, is_created: bool, request: Request) -> None:
+        PersonBase(**data)
