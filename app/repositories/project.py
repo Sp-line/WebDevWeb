e2ef74add68abff8a230e82063ab00920b50a052
spline <dmcemin@gmail.com>
2025-11-13 17:42:10 +0100

    add get_with_order_and_teams to project repository

--- app/repositories/project.py
+++ app/repositories/project.py
@@ -11,9 +11,10 @@ class ProjectRepository(RepositoryBase[Project, ProjectCreate, ProjectUpdate]):
     def __init__(self, session: AsyncSession):
         super().__init__(Project, session)
 
-    async def get_with_order(self, project_id: int) -> Project | None:
+    async def get_with_order_and_teams(self, project_id: int) -> Project | None:
         stmt = select(Project).where(Project.id == project_id).options(
-            selectinload(Project.order)
+            selectinload(Project.order),
+            selectinload(Project.teams)
         )
         result = await self.session.execute(stmt)
         return result.scalar_one_or_none()
