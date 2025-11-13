918ba9d115a9a36f0a1637cafaf1ae2e1b151a77
spline <dmcemin@gmail.com>
2025-11-13 19:35:49 +0100

    add _relation_validate to team

--- app/services/team.py
+++ app/services/team.py
@@ -8,7 +8,7 @@ from repositories.employee import EmployeeRepository
 from repositories.project import ProjectRepository
 from repositories.team import TeamRepository
 from schemas.team import TeamListResponse, TeamCreate, TeamUpdate, TeamDetailResponse, \
-    TeamCreateUpdateResponse
+    TeamCreateUpdateResponse, TeamRelation
 from utils.get_obj_or_404 import get_object_or_404
 
 if TYPE_CHECKING:
@@ -21,6 +21,20 @@ class TeamService:
         self.repo = TeamRepository(session)
         self.project_repo = ProjectRepository(session)
         self.employee_repo = EmployeeRepository(session)
+
+    async def _relation_validate(self, data: TeamRelation) -> None:
+        if data.project_id:
+            await get_object_or_404(self.session, self.project_repo.model, data.project_id)
+
+        if data.teamlead_id:
+            await get_object_or_404(self.session, self.employee_repo.model, data.teamlead_id)
+
+            existing_team = await self.repo.get_team_by_teamlead_id(data.teamlead_id)
+            if existing_team:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail=f"Employee id={data.teamlead_id} is already a teamlead for team id={existing_team.id}"
+                )
 
     async def get_all_with_project_and_teamlead(self) -> TeamListResponse:
         teams = await self.repo.get_all_with_project_and_teamlead()
@@ -42,23 +56,14 @@ class TeamService:
         return TeamDetailResponse.model_validate(team)
 
     async def create(self, data: TeamCreate) -> TeamCreateUpdateResponse:
-        if data.project_id:
-            await get_object_or_404(self.session ,self.project_repo.model, data.project_id)
-
-        if data.teamlead_id:
-            await get_object_or_404(self.session, self.employee_repo.model, data.teamlead_id)
-
-            existing_team = await self.repo.get_team_by_teamlead_id(data.teamlead_id)
-            if existing_team:
-                raise HTTPException(
-                    status_code=status.HTTP_400_BAD_REQUEST,
-                    detail=f"Employee id={data.teamlead_id} is already a teamlead for team id={existing_team.id}"
-                )
+        await self._relation_validate(data)
 
         team = await self.repo.create(data)
         return TeamCreateUpdateResponse.model_validate(team)
 
     async def update(self, team_id: int, data: TeamUpdate) -> TeamCreateUpdateResponse:
+        await self._relation_validate(data)
+
         team = await self.repo.update(team_id, data)
         if not team:
             raise HTTPException(
