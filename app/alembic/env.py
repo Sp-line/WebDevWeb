a96b3a112f1d445e344445fac149a321bd00e6c6
spline <dmcemin@gmail.com>
2025-12-29 14:36:16 +0100

    change: settings import

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -7,7 +7,7 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from app.models import Base
-from config import settings
+from app.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
